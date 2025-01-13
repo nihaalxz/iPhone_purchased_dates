b8dc796054a56c1915aa7fe7a056ac002e511250
nihaalxz <nihaal1010@gmail.com>
2025-01-13 14:45:00 +0530

    minor changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,39 @@
 const date1 = new Date("10/28/2024");
 const date2 = new Date();
 const Difference_In_Time = date2.getTime() - date1.getTime();
-let Difference_In_Days =Math.round(Difference_In_Time / (1000 * 3600 * 24));
-const result= document.getElementById("result");
-result.innerHTML=`<h3>Total number of days iPhone purchased is </h3><h1 style="font-size:76px">${Difference_In_Days}</h1>`;
+let Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24));
+const result = document.getElementById("result");
+result.innerHTML = `<h3>Total number of days iPhone purchased is </h3><h1 style="font-size:76px">${Difference_In_Days}</h1>`;
 
 
 const date3 = new Date("09/03/2018");
 const Difference_In_Time_ = date2.getTime() - date3.getTime();
-let Difference_In_Days_ =Math.round(Difference_In_Time_ / (1000 * 3600 * 24));
-const result_= document.getElementById("result_");
-result_.innerHTML=`<h3>Total number of days Ntorq purchased is </h3><h1 style="font-size:76px">${Difference_In_Days_}</h1>`;
+let Difference_In_Days_ = Math.round(Difference_In_Time_ / (1000 * 3600 * 24));
+const result_ = document.getElementById("result_");
+result_.innerHTML = `<h3>Total number of days Ntorq purchased is </h3><h1 style="font-size:76px">${Difference_In_Days_}</h1>`;
 
 const date4 = new Date("08/31/2021");
 const Difference_In_Time__ = date2.getTime() - date4.getTime();
-let Difference_In_Days__ =Math.round(Difference_In_Time__ / (1000 * 3600 * 24));
-const result__= document.getElementById("result__");
-result__.innerHTML=`<h3>Total number of days HP victus purchased is </h3><h1 style="font-size:76px">${Difference_In_Days__}</h1>`;
+let Difference_In_Days__ = Math.round(Difference_In_Time__ / (1000 * 3600 * 24));
+const result__ = document.getElementById("result__");
+result__.innerHTML = `<h3>Total number of days HP victus purchased is </h3><h1 style="font-size:76px">${Difference_In_Days__}</h1>`;
 
-console.log(Difference_In_Days__);
+const date5 = new Date("12/24/2024");
+const Difference_In_Time__anker = date2.getTime() - date5.getTime();
+let Difference_In_Days__anker = Math.round(Difference_In_Time__anker / (1000 * 3600 * 24));
+const result__anker = document.getElementById("acc_result_1");
+result__anker.innerHTML = `<h3>Total number of anker screen protector </h3><h1 style="font-size:26px;color:white;">${Difference_In_Days__anker}</h1>`;
+
+const date6 = new Date("12/21/2024");
+const Difference_In_Time__ringke = date2.getTime() - date6.getTime();
+let Difference_In_Days__ringke = Math.round(Difference_In_Time__ringke / (1000 * 3600 * 24));
+const result__ringke = document.getElementById("acc_result_2");
+result__ringke.innerHTML = `<h3>Total number of ringke onyx </h3><h1 style="font-size:26px;color:white;">${Difference_In_Days__ringke}</h1>`;
+
+$(document).ready(() => {
+
+    $("#btn-reveal").click(() => {
+        $(".accessory").slideToggle(1000);
+    });
+
+});
