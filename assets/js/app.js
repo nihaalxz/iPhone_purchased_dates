2f56706449220b7792be9e3daf83301e4affd452
nihaalxz <nihaal1010@gmail.com>
2025-01-14 15:45:37 +0530

    minor changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,13 @@ const date2 = new Date();
 const Difference_In_Time = date2.getTime() - date1.getTime();
 let Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24));
 const result = document.getElementById("result");
-result.innerHTML = `<h3>Total number of days iPhone owned:</h3><h1 style="font-size:60px">${Difference_In_Days}</h1>`;
+result.innerHTML = `<h3>Total number of days iPhone owned:</h3><h1 style="font-size:53px">${Difference_In_Days}</h1>`;
 
 const date3 = new Date("09/03/2018");
 const Difference_In_Time_ = date2.getTime() - date3.getTime();
 let Difference_In_Days_ = Math.round(Difference_In_Time_ / (1000 * 3600 * 24));
 const result_ = document.getElementById("result_");
-result_.innerHTML = `<h3>Total number of days Ntorq owned:</h3><h1 style="font-size:60px">${Difference_In_Days_}</h1>`;
+result_.innerHTML = `<h3>Total number of days Ntorq owned:</h3><h1 style="font-size:53px">${Difference_In_Days_}</h1>`;
 
 const date4 = new Date("08/31/2021");
 const Difference_In_Time__ = date2.getTime() - date4.getTime();
@@ -17,7 +17,7 @@ let Difference_In_Days__ = Math.round(
   Difference_In_Time__ / (1000 * 3600 * 24)
 );
 const result__ = document.getElementById("result__");
-result__.innerHTML = `<h3>Total number of days HP victus owned:</h3><h1 style="font-size:60px">${Difference_In_Days__}</h1>`;
+result__.innerHTML = `<h3>Total number of days HP victus owned:</h3><h1 style="font-size:53px">${Difference_In_Days__}</h1>`;
 
 const date5 = new Date("12/24/2024");
 const Difference_In_Time__anker = date2.getTime() - date5.getTime();
